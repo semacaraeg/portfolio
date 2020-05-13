929c480b8181a275852ff22deecfbd70005a69e9
Sarah Macaraeg <semacaraeg@gmail.com>
2020-05-12 18:02:23 -0700

    made some changes on the js file

--- index.js
+++ index.js
@@ -24,6 +24,36 @@ $(document)
             on: 'hover'
          });
 
+
+    // let projects = [
+    //     {
+    //         name: "MSTA-Connect",
+    //         stack:"C#, ASP.Net MVC 5, HTML5/CSS3, Entity Framework, SQL, Azure, CI/CD, Azure DevOps",
+    //         details: "Contributions: Lead developer for the project.",
+    //         image: "msta.png",
+    //         github: "none",
+    //         website: "https://mstaconnect.azurewebsites.net/"
+    //     },
+    //     {
+    //         name: "GOBii",
+    //         stack:"WordPress, WordPress Theme, Bootstrap, PHP, HTML5/CSS3",
+    //         details: "Contributions: Designed and developed the website from ground-up. Participated in stakeholders meeting from ideation to deployment.",
+    //         image: "gobii.png",
+    //         github: "none",
+    //         website: "https://gobiiproject.org/"
+    //     }
+    // ];
+
+    // let root = document.querySelector("#projects-root");
+
+        // let skills = document.querySelector("#skills-section").onmouseover = 
+         //   function(){
+          //      this.a
+           // };
+         
+
+
+
     })
   ;
 
