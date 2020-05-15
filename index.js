09ba79eac9d76103bd6bf49083e731dbee2805b4
Sarah Macaraeg <semacaraeg@gmail.com>
2020-05-14 18:20:01 -0700

    cleaned-up

--- index.js
+++ index.js
@@ -1,76 +1,38 @@
-$(document)
-    .ready(function() {
+$(document).ready(function () {
+  // fix menu when passed
+  $(".masthead").visibility({
+    once: false,
+    onBottomPassed: function () {
+      $(".fixed.menu").transition("fade in");
+    },
+    onBottomPassedReverse: function () {
+      $(".fixed.menu").transition("fade out");
+    },
+  });
 
-      // fix menu when passed
-      $('.masthead')
-        .visibility({
-          once: false,
-          onBottomPassed: function() {
-            $('.fixed.menu').transition('fade in');
-          },
-          onBottomPassedReverse: function() {
-            $('.fixed.menu').transition('fade out');
-          }
-        })
-      ;
+  // create sidebar and attach to menu open
+  $(".ui.sidebar").sidebar("attach events", ".toc.item");
 
-      // create sidebar and attach to menu open
-      $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item')
-      ;
+  $(".special.cards .image").dimmer({
+    on: "hover",
+  });
+//added event listener to the project cards
+  let projects = document.querySelector("#work");
+  projects.addEventListener("mouseenter", function () {
+    $(".special.cards .card").transition({
+      animation: "jiggle",
+      duration: 600,
+      interval: 100,
+    });
+  });
 
-      $('.special.cards .image')
-        .dimmer({
-            on: 'hover'
-         });
+  //added event listener to the skills bar
+  let skills = document.querySelector("#skills-section");
+  skills.addEventListener("mouseenter", function () {
+    $("li .bar").transition({
+      animation: "html 2s",
+    });
+  });
 
-
-    // let projects = [
-    //     {
-    //         name: "MSTA-Connect",
-    //         stack:"C#, ASP.Net MVC 5, HTML5/CSS3, Entity Framework, SQL, Azure, CI/CD, Azure DevOps",
-    //         details: "Contributions: Lead developer for the project.",
-    //         image: "msta.png",
-    //         github: "none",
-    //         website: "https://mstaconnect.azurewebsites.net/"
-    //     },
-    //     {
-    //         name: "GOBii",
-    //         stack:"WordPress, WordPress Theme, Bootstrap, PHP, HTML5/CSS3",
-    //         details: "Contributions: Designed and developed the website from ground-up. Participated in stakeholders meeting from ideation to deployment.",
-    //         image: "gobii.png",
-    //         github: "none",
-    //         website: "https://gobiiproject.org/"
-    //     }
-    // ];
-
-        let projects = document.querySelector("#work");
-        projects.addEventListener('mouseenter',  
-            function(){
-                $('.special.cards .card')
-                .transition({
-                    animation : 'jiggle',
-                    duration  : 600,
-                    interval  : 100
-               })
-               ;
-        
-            });
-
-            let skills = document.querySelector("#skills-section");
-        skills.addEventListener('mouseenter',  
-            function(){
-                $('li .bar')
-                .transition({
-                    animation : 'html 2s',
-               })
-               ;
-        
-            });
-         
-
-
-//closing tag for document.ready
-    })
-  ;
-
+  //closing tag for document.ready
+});
